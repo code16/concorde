579f50e671c0a3d853c0779a9d5f0faf489c26c0
antoine <guingand.antoine@gmail.com>
2025-12-08 15:04:26 +0100

    update embed image ratio

--- resources/views/vendor/ozu/components/embeds/ozu-content-image.blade.php
+++ resources/views/vendor/ozu/components/embeds/ozu-content-image.blade.php
@@ -9,10 +9,20 @@
 
 <div class="mb-14 md:mb-18 -mx-7.5 md:mx-0 flex flex-col items-center">
     <figure class="md:rounded-2xl md:p-2 md:bg-neutral-50 md:border border-neutral-200"
-        x-data="{ imgWidth: 0 }" :style="{ '--img-width': `${imgWidth}px` }">
-        <img class="max-w-full object-cover max-h-100 lg:max-h-128 border-y md:border-x md:rounded-xl overflow-hidden border-neutral-200" src="{{ $media->thumbnail(1400) }}" alt="{{ $legend ?? '' }}"
-            x-init="imgWidth = $width" x-resize="imgWidth = $width"
-        >
+        x-data="{ imgWidth: 0 }"
+        :style="{ '--img-width': `${imgWidth}px` }"
+    >
+        @if($thumbnail = $media->thumbnail(1400, 1024))
+            <img class="min-h-50 max-w-full object-cover max-h-100 lg:max-h-128 border-y md:border-x md:rounded-xl overflow-hidden border-neutral-200"
+                width="{{ $thumbnail->width() }}"
+                height="{{ $thumbnail->height() }}"
+                src="{{ $thumbnail }}"
+                alt="{{ $legend ?? '' }}"
+                loading="lazy"
+                x-init="imgWidth = $width"
+                x-resize="imgWidth = $width"
+            >
+        @endif
         @if($legend)
             <figcaption class="min-w-0 max-w-(--img-width) text-sm text-neutral-600 mt-3 mb-2 px-7.5 md:px-4">
                 {{ $legend }}
